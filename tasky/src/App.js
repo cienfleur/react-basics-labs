263dd57132e3cc70f1842f118cf4cb9812fcbc75
cienfleur <redvelvet0214@protonmail.ch>
2024-10-04 14:15:53 +0100

    Tasky Lab 2 - Exercises done

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -8,9 +8,9 @@ import { v4 as uuidv4 } from 'uuid';
 function App() {
   const [ taskState, setTaskState ] = useState({
     tasks: [
-      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today", done: false },
-      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", done: false },
-      { id: 3, title: "Tidy up", deadline: "Today", done: false}
+      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today", done: false, priority: "Low" },
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", done: false, priority: "Medium" },
+      { id: 3, title: "Tidy up", deadline: "Today", done: false, priority: "High"}
     ]
   });
   const doneHandler = (taskIndex) => {
@@ -27,7 +27,8 @@ function App() {
   const [ formState, setFormState ] = useState({
     title: "",
     description: "",
-    deadline: ""
+    deadline: "",
+    priority: "Low"
   });
   const formChangeHandler = (event) => {
     let form = {...formState};
@@ -41,6 +42,9 @@ function App() {
           break;
       case "deadline":
           form.deadline = event.target.value;
+          break;
+      case "priority":
+          form.priority = event.target.value;
           break;
       default:
           form = formState;
@@ -67,6 +71,7 @@ function App() {
         title={task.title}
         description={task.description}
         deadline={task.deadline}
+        priority={task.priority}
         key={task.id}
         done={task.done}
         markDone={() => doneHandler(index)}
@@ -74,7 +79,7 @@ function App() {
       />
     ))}
     <AddTaskForm submit={formSubmitHandler} change={formChangeHandler} />
-    console.log(formState);
+      console.log(formState);
   </div>
   );
 }
